3d696b65120f19dd7690cc14553899401a8fdf80
lrnbrad <lrnbrad@gmail.com>
2023-11-23 17:04:30 +0800

    feat(project): complete P13

--- P13/source/main.cpp
+++ P13/source/main.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void cubeByReference(int *nPtr) {
-	*nPtr = *nPtr * *nPtr * *nPtr;
+void cubeByReference(int &nPtr) {
+	nPtr = nPtr * nPtr * nPtr;
 }
 
 int main() {
 	int number = 5;
 	printf("The original value of number is %d", number);
 
-	cubeByReference(&number);
+	cubeByReference(number);
 
 	printf("\nThe new value of number is %d\n", number);
 	return 0;
